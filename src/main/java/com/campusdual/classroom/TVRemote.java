f7fbc236bffd10c8fa2b2367afc25c6aaf042e41
Chus <mjesus.mh.12@gmail.com>
2024-09-27 13:22:28 +0200

    Ejr.: 11 completado

--- src/main/java/com/campusdual/classroom/TVRemote.java
+++ src/main/java/com/campusdual/classroom/TVRemote.java
@@ -2,4 +2,46 @@ package com.campusdual.classroom;
 
 public class TVRemote {
 
+    public Boolean on = false;
+    public Integer channel = 0;
+    public Integer volume = 0;
+
+    private String color;
+
+    public TVRemote(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    void turnOn() {
+        this.on = true;
+    }
+
+    void turnOff() {
+        this.on = false;
+    }
+
+    void channelUp() {
+        this.channel++;
+    }
+
+    void channelDown() {
+        if (this.channel > 0) {
+            this.channel--;
+        }
+    }
+
+    void volumeUp() {
+        this.volume++;
+    }
+
+    void volumeDown() {
+        if (this.volume > 0) {
+            this.volume--;
+        }
+    }
+
 }
